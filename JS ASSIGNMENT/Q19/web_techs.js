03e952541ea5a5b64bd5434ea6dfdebf1f4a77f7
Ronak Javia <118874605+ronakjavia@users.noreply.github.com>
2023-02-17 22:28:57 +0530

    Update web_techs.js

--- JS ASSIGNMENT/Q19/web_techs.js
+++ JS ASSIGNMENT/Q19/web_techs.js
@@ -1,2 +1,4 @@
-export const web_techs =
-  'Yii, Meteor JS, Django, Motion UL, Blockchain, Laravel, Serverless Architecture, Data Security, API-firs,  Development'
+module.exports = {
+  web_techs:
+    'Yii, Meteor JS, Django, Motion UL, Blockchain, Laravel, Serverless Architecture, Data Security, API-firs,  Development',
+}
